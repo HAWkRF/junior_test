64001fb7d2c5ec5d26ce69ad474d6f0d9a7cb574
Pavel <hawkrammfan@gmail.com>
2021-10-23 20:14:58 +0300

    add Main component

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,7 +1,9 @@
 import Music from './components/Music.vue'
+import Main from './components/Main'
 
 let url = process.env.MIX_APP_PATH;
 
 export const routes = [
-  { path: url + 'music', name: 'music', component: Music },
+    { path: url + 'music', name: 'music', component: Music },
+    { path: url, name: 'main', component: Main },
 ];
