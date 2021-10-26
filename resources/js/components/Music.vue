33de8ca3efac68d6ec9d31d1b6bed0a3c344ca63
Pavel <hawkrammfan@gmail.com>
2021-10-26 21:07:26 +0300

    add title song on form find

--- resources/js/components/Music.vue
+++ resources/js/components/Music.vue
@@ -48,9 +48,11 @@
 
         <b-modal id="music-modal-find" centered hide-footer>
             <div class="modal-center d-flex flex-column text-center mx-auto">
-                <div class="form-block">
+                <div class="form-block-find">
                     <img class="imgSong" :src="this.iconSong"/>
+                    <p class="link-tik-tok">Ссылка на звук в TikTok</p>
                     <input type="text" class="form-control" placeholder="Ссылка на звук в TikTok" v-model="val" required="" />
+                    <p class="title-song">Название</p>
                     <input type="text" class="form-control" placeholder="Название трека" v-model="this.title" required="" />
                     <input type="text" class="form-control" placeholder="Исполнитель трека" v-model="this.nickname" required="" />
                     <input type="text" class="form-control" placeholder="Альбом трека" v-model="this.album" required="" />
