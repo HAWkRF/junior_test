28a02a312937fc69fcb5a4ffb4626ff28e4a8c7a
Pavel <hawkrammfan@gmail.com>
2021-10-31 20:21:24 +0300

    remove route for Main component

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,9 +1,7 @@
 import Music from './components/Music.vue'
-import Main from './components/Main'
 
 let url = process.env.MIX_APP_PATH;
 
 export const routes = [
     { path: url + 'music', name: 'music', component: Music },
-    { path: url, name: 'main', component: Main },
 ];
